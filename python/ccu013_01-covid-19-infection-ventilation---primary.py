7f3639a6572d559d926617cd5c6dfa164ea51a29
Martin Chapman <contact@martinchapman.co.uk>
2024-06-04 16:10:47 +0100

    Update made by Phenoflow

---
+++ python/ccu013_01-covid-19-infection-ventilation---primary.py
@@ -0,0 +1,25 @@
+# Johan H Thygesen, Christopher Tomlinson, et al., 2024.
+
+import sys, csv, re
+
+codes = [{"code":"E85.1","system":"snomedct"},{"code":"E85.2","system":"snomedct"}];
+REQUIRED_CODES = 1;
+with open(sys.argv[1], 'r') as file_in, open('ccu013_01-covid-19-infection-potential-cases.csv', 'w', newline='') as file_out:
+    csv_reader = csv.DictReader(file_in)
+    csv_writer = csv.DictWriter(file_out, csv_reader.fieldnames + ["ccu013_01-covid-19-infection-ventilation---primary-identified"])
+    csv_writer.writeheader();
+    codes_identified = 0;
+    for row in csv_reader:
+        newRow = row.copy();
+        for cell in row:
+            # Iterate cell lists (e.g. codes)
+            for item in re.findall(r'\(([^,]*)\,', row[cell]):
+                if(item in list(map(lambda code: code['code'], codes))): codes_identified+=1;
+                if(codes_identified>=REQUIRED_CODES):
+                    newRow["ccu013_01-covid-19-infection-ventilation---primary-identified"] = "CASE";
+                    break;
+            if(codes_identified>=REQUIRED_CODES): break;
+        if(codes_identified<REQUIRED_CODES):
+            newRow["ccu013_01-covid-19-infection-ventilation---primary-identified"] = "UNK";
+        codes_identified=0;
+        csv_writer.writerow(newRow)
